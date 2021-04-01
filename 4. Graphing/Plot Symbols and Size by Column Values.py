ea080c26ab66daadec64d015b0da8b24829dead8
CP <cp@originlab.com>
2021-04-01 17:57:48 -0400

    change to modi_col and show symbol_fill

--- 4. Graphing/Plot Symbols and Size by Column Values.py
+++ 4. Graphing/Plot Symbols and Size by Column Values.py
@@ -4,16 +4,18 @@ as the symbol kind and col(D) as size
 '''
 import originpro as op
 wks = op.new_sheet();
-wks.cols=4
+wks.cols=5
 x=[x for x in range(1,5)]
-wks.from_list(0,x)
-wks.from_list(1,x)
-wks.from_list(2,x)
-x=[10*x for x in range(1,5)];
-wks.from_list(3,x)
+for i in range(5):
+    wks.from_list(i,x)
+
 graph = op.new_graph(template='scatter')
 layer = graph[0]
 plot = layer.add_plot(wks, coly=1, colx=0)
 layer.rescale()
-plot.symbol_kind = op.modifier_col()
-plot.symbol_size = op.modifier_col(2)
+plot.symbol_kind = op.modi_col(1)
+plot.symbol_size = op.modi_col(2)
+plot.symbol_sizefactor=10
+plot.symbol_fill = op.modi_col(3)
+
+

5ddfb786367f6fbd2b9c3dc6efffe67ff5f41d55
Yiming <mynehc@gmail.com>
2021-04-02 11:59:34 -0400

    Create Control Plots As Group.py
    Add Control Plots As Group.py

---
+++ 4. Graphing/Control Plots As Group.py
@@ -0,0 +1,19 @@
+'''
+This sample plots a whole worksheet as XY plot and control plots in group
+'''
+import os
+import originpro as op
+
+wks = op.new_sheet()
+wks.from_file(os.path.join(op.path('e'), 'Samples', 'Graphing', 'Group.dat'))
+graph = op.new_graph(template='scatter')
+gl=graph[0]
+
+# plot whole sheet as XY plot
+plot = gl.add_plot(f'{wks.lt_range()}!(?,1:end)')
+
+# group the plots and control plots setting in group
+gl.group()
+plot.colormap = 'Candy'
+plot.shapelist = [3, 2, 1]
+gl.rescale()

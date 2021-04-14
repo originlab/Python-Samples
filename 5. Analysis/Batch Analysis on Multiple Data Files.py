334d26ab44fb8b5c89de69c128e107bf79d6ac07
Yiming <mynehc@gmail.com>
2021-04-14 14:10:15 -0400

    Batch peak fitting on Multiple Files added

---
+++ 5. Analysis/Batch Analysis on Multiple Data Files.py
@@ -0,0 +1,46 @@
+'''
+This sample is to show how to run gaussian peak fit on multiple data files. 
+Then the fitted peak parameters xc, w, A are plotted as a function of temperature
+in a three Y axes graph.
+'''
+
+import originpro as op
+import numpy as np
+
+# One worksheet for imported data, one worksheet for peak fitting result
+wks = op.new_sheet()
+wks_result = op.new_sheet()
+
+
+wts = [0, 25, 100, 150, 200]
+for i, wt in enumerate(wts):    
+    # Import each data file into wks
+    fd = op.path('e') + 'Samples\Batch2'
+    fn = f'{fd}\Sample 2 {wt} wt.txt'
+    wks.from_file(fn, keep_DC=False)
+    
+    # Baseline subtraction
+    spec = wks.to_list(1)
+    spec = np.array(spec)
+    base = np.linspace(spec[0], spec[-1], len(spec)) # straight line connecting start and end point as baseline
+    spec = spec - base
+    wks.from_list(1, spec)      
+    
+    # Run Gaussian fit on subtracted spectrum
+    model = op.NLFit('Gaussian')
+    model.set_data(wks, 0, 1)
+    model.fix_param('y0',0)
+    model.fit()
+    ret = model.result()
+    
+    # Fitting result is saved to each row of wks_result
+    wks_result.from_list2([wt, ret['xc'], ret['w'], ret['A'], ret['adjr']], i) 
+    
+wks_result.set_labels(['temperature','fit x','fit width', 'fit area', 'Adj. R-Square'])
+
+# Plot the data
+gp = op.new_graph(template='3Ys_Y-YY')
+for i in range(3):    
+    gp[i].add_plot(wks_result,1+i,0)
+    gp[i].rescale()
+

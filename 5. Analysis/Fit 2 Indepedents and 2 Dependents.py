194f2d8689a25383cf1741d8460a2feed17fbc91
maxkeiko <max@originlab.com.cn>
2021-04-15 14:32:01 +0800

    use a new sample data

--- 5. Analysis/Fit 2 Indepedents and 2 Dependents.py
+++ 5. Analysis/Fit 2 Indepedents and 2 Dependents.py
@@ -1,19 +1,12 @@
 import originpro as op
 wks = op.new_sheet()
-fn=op.path('e') + 'Samples\Curve Fitting\Enzyme.dat'
+fn=op.path('e') + 'Samples\Curve Fitting\Enzyme2.dat'
 wks.from_file(fn, False)
-#add two more columns since function requires x to be 1/[S] and y to be 1/V.
-#and make the two new columns as XY and set the formula S=col(A), V=col(C)
-wks.cols=5
-wks.cols_axis('xy',3)
-wks.set_formula(3, '1/A')
-wks.set_formula(4, '1/C')
-wks.set_labels(['x2', 'v2'], col=3)
+wks.cols_axis('x',2,2)
 
 #Specify the input data range, which follows 
 #the independent/dependent order in FDF (x1, x2, v1, v2)
-range=f'{wks.lt_range()}!(1,4,2,5)'
-
+range=f'{wks.lt_range()}!(1,3,2,4)'
 model = op.NLFit('HillBurk')
 model.set_range(range)
 model.fit()

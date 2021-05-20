50a1ae808ee83e6d9d94d8bc013377d8a35e0ff6
CP <cp@originlab.com>
2021-05-19 22:48:56 -0400

    wks.userprops and put back

--- 1. Origin-Python Data Exchange/Worksheet User Tree Access.py
+++ 1. Origin-Python Data Exchange/Worksheet User Tree Access.py
@@ -1,6 +1,6 @@
 '''
-This sample shows how to set values to worksheet tree nodes, and 
-then read out into a tree object. This is useful to save the metadata
+This sample shows how to prepare a worksheet tree, and 
+how to read the values back. This is useful to save the metadata
 of a data file. See the blog:
 https://blog.originlab.com/accessing-metadata-using-python
 '''
@@ -13,8 +13,22 @@ wks.set_str("tree.data.name", "Larry")
 wks.set_int("tree.data.age", 37)
 wks.set_float("tree.data.mean", 23.56)
 
-# Save the worksheet tree values to a tree object and print out the values.
+# access the tree as a dictionary
+dd = wks.userprops['data']
+user=dd['name']
+value=dd['age']
+print(f'{user} is {value}')
+
+# you can also get the tree as an xml ElementTree for more advanced usage
 trWks = wks.usertree
 trData = trWks.find('data')
 for child in trData:
     print(f'{child.tag} = {child.text}')
+    
+# how to put a modified tree to another sheet
+wk2 = op.new_sheet()
+age_node=trData.find('age')
+age_node.text='47'
+
+wk2.usertree=trWks
+print(wk2.userprops['data']['age'])

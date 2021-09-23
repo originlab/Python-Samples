e461d364207f8b9e98624d42207ae8aa18e5cf4f
CP <cp@originlab.com>
2021-09-22 21:12:50 -0400

    add showing palette

--- 6. Images/Accessing Individual Image in an Image Stack.py
+++ 6. Images/Accessing Individual Image in an Image Stack.py
@@ -6,10 +6,12 @@ fn = op.path('e') + r'Samples\Image Processing and Analysis\*.tif'
 iw=op.new_image()
 iw.from_file(fn)
 print(iw.frames)
-#get the 3 image
+#get the 3rd image
 im2 = iw.to_np2d(2)
 im2 *= 2
 im2 = invert(im2)
 #put it back into 2nd image
 iw.from_np2d(im2, 1)
+#show thumbnails
 iw.set_int('NAV',1)
+iw.set_str('Palette', 'Fire')

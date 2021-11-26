75b1a1ba482ed1e74fcb14a390153077ec3cf443
CP <cp@originlab.com>
2021-11-26 14:21:56 -0500

    add code to show how to set color scale title from using the matrix's long name

--- 4. Graphing/Plot Heatmap from Matrix Data.py
+++ 4. Graphing/Plot Heatmap from Matrix Data.py
@@ -1,13 +1,16 @@
 '''
 This sample shows how to plot a heatmap from matrix data 
-It also shows how to set matrix xy map and how to set plot colormap
+It also shows how to set matrix xy map, color scale title and how to set plot colormap
 '''
 import originpro as op
 import numpy as np
 
-arr = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+arr = np.array([[1, -8], [3, -4], [5, 4], [7, 8]])
 mat = op.new_sheet(type='m')
 mat.from_np(arr)
+#set the 1st matrix's long name, which is the Z lable, which the color scale will use as title
+mat.set_label(0, 'Z Levels')
+#set the XY mapping in the matrix which will provde the XY scale for the graph
 x1,x2,y1,y2 = 0.5, 2.5, 0.5, 4.5
 mat.xymap = x1,x2,y1,y2
 gr = op.new_graph(template='Heat_Map.otpu')
@@ -19,7 +22,7 @@ g.set_ylim(y1-hs, y2+hs, 1)
 g.set_xlim(x1-hs, x2+hs, 1)
 #set plot colormap
 z = p.zlevels
-z['minors'] = 2
-z['levels'] = [1, 3,7, 8]
+z['minors'] = 15
+z['levels'] = [-8, 0, 8]
 p.zlevels = z
 p.colormap = 'BlueYellow.pal'

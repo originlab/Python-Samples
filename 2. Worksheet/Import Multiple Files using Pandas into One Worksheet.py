73d7f5341fadee43acd4c84dbae3968edab10f5b
CP <cp@originlab.com>
2022-02-07 12:44:10 -0500

    update some comments, especially about removing 'Unnamed: 0_level_2'

--- 2. Worksheet/Import Multiple Files using Pandas into One Worksheet.py
+++ 2. Worksheet/Import Multiple Files using Pandas into One Worksheet.py
@@ -1,21 +1,5 @@
-# Usage
-# =====
-# Script concatenates multiple NetCDF files into one file.
-# Instructions:
-#   1. Put all *.NC files you want to process into an input folder.
-#   2. Run this script.
-#   3. You will be prompted for an input folder.
-#   4. You will then be prompted for an output file.
-#       Do NOT choose the input folder.
-#
-# Note: If large number of files, it may take quite some time to run.
-#
-# Note: All files to concatenate must have the same structure and
-#   and variable dimensions. Also, all files must have a dimension
-#   such as `time` in order for the concatenation to work.
-
 """
-Simple example of doing a non-linear fitting without creating report sheet
+This shows import text files using DataFrame instead of Origin's internal import
 """
 import originpro as op
 import pandas as pd
@@ -26,8 +10,10 @@ col = 0
 for file in os.listdir(fd):
     df = pd.read_table(os.path.join(fd, file), header=[0,1,2])    
     wks.from_df(df, col)
+    wks.cols_axis('xy', col)
     wks.set_labels(df.columns.get_level_values(0), 'L', col)
     wks.set_labels(df.columns.get_level_values(1), 'U', col)
+    #the sample data file has no comment for X column, so we need to remove the 'Unnamed: 0_level_2'
     comments = df.columns.get_level_values(2).tolist()
     comments[0] = ""
     wks.set_labels(comments, 'C', col)

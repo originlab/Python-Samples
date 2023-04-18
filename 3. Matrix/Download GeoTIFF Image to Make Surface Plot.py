225a173eed12c3ba7d5a132bb34ebd11fb46653f
CP <cp@originlab.com>
2023-04-18 10:45:05 -0400

    add iw.destropy as it is no longer needed to make the graph

--- 3. Matrix/Download GeoTIFF Image to Make Surface Plot.py
+++ 3. Matrix/Download GeoTIFF Image to Make Surface Plot.py
@@ -5,13 +5,16 @@ Image Window, it can be converted to a matrix with the coordinates from the GeoT
 The sample also shows to download a graph template from Originlab to make our plot
 '''
 import originpro as op
-#use the smaller 2 arc sec images available only for Alaska on the National Map
+#use the smaller 2-arc sec DEM available only for Alaska from National Map
 url = r'https://prd-tnm.s3.amazonaws.com/StagedProducts/Elevation/2/TIFF/historical/n59w154/USGS_2_n59w154_20210401.tif'
 iw=op.new_image()
 iw.from_file(url)
 #create an empty matrix and tranfer the image to it
 mat = op.new_book('m')
 op.lt_exec(f'cv2mat img:=[{iw.name}]1! mat:=[{mat.name}]1!')
+#you may want to keep the image window and comment out the next line but 
+#it is no longer needed to make the graph
+iw.destroy()
 #make sure you download the template from Template Center first
 gp = op.new_graph(template='RaisedReliefMap')
 
@@ -22,7 +25,7 @@ gl.set_int('MatMaxPtsEnabled', 1)
 plot = gl.add_mplot(mat[0], 0)
 gl.rescale()
 
-#can comment out later when template file is updated
+#if template did not have clip data enabled
 #gl.set_int('clip',1)
 
 plot.colormap = 'Magma.PAL'

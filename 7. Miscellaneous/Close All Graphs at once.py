609aa7fadab6f5777a63494ef1f757b852e3f1b7
jasmine <jasmine@originlab.com.cn>
2024-05-31 16:53:58 +0800

    ORG-4678 add example

---
+++ 7. Miscellaneous/Close All Graphs at once.py
@@ -0,0 +1,8 @@
+'''
+This sample shows how to close all graphs at once
+'''
+import originpro as op
+
+# doc -e: execute the script in {} for all graph window (LP)
+# win -cp: post a message to close the window
+op.lt_exec("doc -e LP {win -cp %H}")
